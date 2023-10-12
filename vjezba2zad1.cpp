67f6ed86ea0eb849a83fa0dfb901a2501c3cc2e3
DenisAs1234 <147091796+DenisAs1234@users.noreply.github.com>
2023-10-12 20:11:54 +0200

    Add files via upload

---
+++ vjezba2zad1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int* min_max(int niz[], int& a, int& b)
+{
+	int i;
+	for (i = 0; i < 6; i++)
+	{
+		if (niz[i] < a)
+			a = niz[i];
+		if (niz[i] > b)
+			b = niz[i];
+	}
+	return &a, &b;
+}
+
+int main()
+{
+	int niz[] = { 4,6,1,8,0,2 };
+	int min = niz[0];
+	int max = niz[0];
+	min_max(niz, min, max);
+	cout << "Najmanji je" << min << endl;
+	cout << "Najveći je" << max << endl;
+
+}
