0c8da67bf1dbe9b99acf0c21143f721fd2e2b3b9
DenisAs1234 <147091796+DenisAs1234@users.noreply.github.com>
2023-11-04 17:20:16 +0000

    Update zad3.cpp

--- vjezba2/zad3.cpp
+++ vjezba2/zad3.cpp
@@ -6,92 +6,90 @@ struct vect
 	int logv;
 	int fiz;
 	int* niz;
+
+	void vector_new(int vel)
+	{
+		niz = new int[vel];
+	}
+
+	void vector_delete()
+	{
+		delete niz;
+	}
+
+	int* vector_push_back(const int& novi)
+	{
+		niz[logv] = novi;
+		logv++;
+		return niz;
+	}
+
+	void vector_pop_back()
+	{
+		logv--;
+	}
+
+	int& vector_front()
+	{
+		return niz[0];
+	}
+
+	int& vector_back()
+	{
+		return niz[logv - 1];
+	}
+
+	unsigned int vector_size()
+	{
+		return logv;
+	}
 };
-
-vect v, nv;
-
-void vector_new(int vel)
-{
-	v.niz = new int[vel];
-}
-
-void vector_delete()
-{
-	delete v.niz;
-	delete nv.niz;
-}
-
-int* vector_push_back(const int& novi)
-{
-	int i;
-	if (v.logv == v.fiz)
-	{
-		nv.niz = new int[2 * v.fiz];
-	}
-	v.fiz = 2 * v.fiz;
-	for (i = 0; i < v.fiz; i++)
-	{
-		nv.niz[i] = v.niz[i];
-	}
-	nv.niz[v.logv] = novi;
-	v.logv++;
-	return nv.niz;
-}
-
-void vector_pop_back()
-{
-	v.logv--;
-}
-
-int& vector_front()
-{
-	return nv.niz[0];
-}
-
-int& vector_back()
-{
-	return nv.niz[v.logv - 1];
-}
-
-unsigned int vector_size()
-{
-	return v.fiz;
-}
 
 int main()
 {
+	vect v, nv;
 	cout << "Fizicka velicina: " << endl;
 	cin >> v.fiz;
 	v.logv = 0;
-	v.niz = new int[v.fiz];
+	v.vector_new(v.fiz);
 	int i;
 	for (i = 0; i < v.fiz; i++)
 	{
 		cin >> v.niz[i];
 		v.logv++;
 	}
+	if (v.logv == v.fiz)
+	{
+		nv.vector_new(2 * v.fiz);
+	}
+	for (i = 0; i < v.fiz; i++)
+	{
+		nv.niz[i] = v.niz[i];
+	}
 	const int x = 10;
-	int* noviniz = vector_push_back(x);
+	int* noviniz = v.vector_push_back(x);
 	cout << "Povecani niz:" << endl;
 	for (i = 0; i < v.logv; i++)
 	{
 		cout << "" << noviniz[i] << " ";
 	}
 	cout << endl;
-	vector_pop_back();
+	nv.logv = v.logv;
+	nv.vector_pop_back();
 	cout << "Izbrisan zadnji element:" << endl;
-	for (i = 0; i < v.logv; i++)
+	for (i = 0; i < nv.logv; i++)
 	{
-		cout << "" << v.niz[i] << " ";
+		cout << "" << nv.niz[i] << " ";
 	}
 	cout << endl;
-	int y = vector_front();
+	int y = nv.vector_front();
 	cout << "Prvi element: " << y << "" << endl;
-	int z = vector_back();
+	int z = nv.vector_back();
 	cout << "Zadnji element: " << z << "" << endl;
-	int n = vector_size();
+	int n = nv.vector_size();
 	cout << "Velicina niza: " << n << "" << endl;
-	vector_new(v.fiz);
-	vector_delete();
+	v.vector_delete();
+	nv.vector_delete();
 	return 0;
 }
+
