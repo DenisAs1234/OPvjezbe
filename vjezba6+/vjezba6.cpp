0b13a8704ad817b702e9c1383f3e0ec1404ae1c3
DenisAs1234 <147091796+DenisAs1234@users.noreply.github.com>
2023-11-30 20:48:08 +0100

    Add files via upload

---
+++ vjezba6+/vjezba6.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include "klase.hpp"
+using namespace std;
+
+int main()
+{
+	Owner vl1;
+	Owner vl2;
+	int br_ljub;
+	cout << "Broj ljubimaca: ";
+	cin >> br_ljub;
+	vector<VirtualPet> ljubimci1 = vl1.getLjubimci();
+	vector<VirtualPet> ljubimci2 = vl2.getLjubimci();
+	vl1.getLjubimci().resize(br_ljub);
+	vl2.getLjubimci().resize(br_ljub);
+	string ime_vl1;
+	string ime_vl2;
+	cout << "Vlasnik 1: " << endl;
+	cin >> ime_vl1;
+	vl1.unos_ljubimaca(ljubimci1);
+	cout << "Vlasnik 2:" << endl;
+	cin >> ime_vl2;
+	vl2.unos_ljubimaca(ljubimci2);
+	int br_akc;
+	cout << "Broj akcija: ";
+	cin >> br_akc;
+	vl1.set(ime_vl1, br_akc);
+	vl2.set(ime_vl2, br_akc);
+	vector<string> niz_akc1 = vl1.getNizAkcija();
+	vector<string> niz_akc2 = vl2.getNizAkcija();
+	vl1.getNizAkcija().resize(2 * vl1.getBrAkcija());
+	vl2.getNizAkcija().resize(2 * vl2.getBrAkcija());
+	vl1.getPomNiz().resize(2 * vl1.getBrAkcija());
+	vector<int> pom_niz1 = vl1.getPomNiz();
+	vector<int> pom_niz2 = vl2.getPomNiz();
+	vl1.niz_indeksa(pom_niz1);
+	vl2.getPomNiz().resize(2 * vl2.getBrAkcija());
+	vl2.niz_indeksa(pom_niz2);
+	int i;
+	srand(time(NULL));
+	for (i = 0; i < br_ljub; i++)
+	{
+		vl1.redoslijed_akcija(pom_niz1, niz_akc1);
+		cout << vl1.getLjubimci().at(i).getIme() << endl;
+		vl1.ispis_niza_akcija(niz_akc1);
+		vl2.redoslijed_akcija(pom_niz2, niz_akc2);
+		cout << vl2.getLjubimci().at(i).getIme() << endl;
+		vl2.ispis_niza_akcija(niz_akc2);
+		vl1.obavljanje_akcija(vl1.getLjubimci().at(i), niz_akc1);
+		cout << ime_vl1 << " - " << vl1.getLjubimci().at(i).getIme() << endl;
+		vl1.getLjubimci().at(i).ispis_bodova(vl1.getLjubimci().at(i).getGlad(), vl1.getLjubimci().at(i).getSreca());
+		vl2.obavljanje_akcija(vl2.getLjubimci().at(i), niz_akc2);
+		cout << ime_vl2 << " - " << vl2.getLjubimci().at(i).getIme() << endl;
+		vl2.getLjubimci().at(i).ispis_bodova(vl2.getLjubimci().at(i).getGlad(), vl2.getLjubimci().at(i).getSreca());
+	}
+	return 0;
+}
